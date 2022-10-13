e8059c4918377da047fd07f5d2b747ab1dcfe928
ArchUsr64 <ArchUsr64>
2022-10-13 15:42:46 +0530

    UARTs working

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,29 @@
 #![no_std]
 #![no_main]
 
-// GPIO traits
 use embedded_hal::digital::v2::OutputPin;
-
-// Ensure we halt the program on panic (if we don't mention this crate it won't
-// be linked)
+use fugit::HertzU32;
+use hal::gpio::pin::bank0::{Gpio0, Gpio1};
+use hal::uart::{DataBits, StopBits, UartConfig};
 use panic_halt as _;
-
-// Pull in any important traits
-use rp_pico::hal::prelude::*;
-
-// A shorter alias for the Peripheral Access Crate, which provides low-level
-// register access
-use rp_pico::hal::pac;
-
-// A shorter alias for the Hardware Abstraction Layer, which provides
-// higher-level drivers.
 use rp_pico::entry;
+use rp_pico::hal::pac;
+use rp_pico::hal::prelude::*;
+type UartPins = (
+	hal::gpio::Pin<Gpio0, hal::gpio::Function<hal::gpio::Uart>>,
+	hal::gpio::Pin<Gpio1, hal::gpio::Function<hal::gpio::Uart>>,
+);
+type Uart = hal::uart::UartPeripheral<hal::uart::Enabled, pac::UART0, UartPins>;
 use rp_pico::hal;
+
 #[entry]
 fn main() -> ! {
+	pub const CUSTOM_CONFIG: UartConfig = UartConfig {
+		baudrate: HertzU32::from_raw(115_200),
+		data_bits: DataBits::Eight,
+		stop_bits: StopBits::One,
+		parity: None,
+	};
 	let mut pac = rp_pico::hal::pac::Peripherals::take().unwrap();
 	let core = rp_pico::hal::pac::CorePeripherals::take().unwrap();
 	let mut watchdog = rp_pico::hal::Watchdog::new(pac.WATCHDOG);
@@ -44,12 +47,17 @@ fn main() -> ! {
 		sio.gpio_bank0,
 		&mut pac.RESETS,
 	);
-	let mut value = 100;
-	let mut led = pins.led.into_push_pull_output();
+	let uart_pins = (
+		pins.gpio0.into_mode::<hal::gpio::FunctionUart>(),
+		pins.gpio1.into_mode::<hal::gpio::FunctionUart>(),
+	);
+	let mut uart = hal::uart::UartPeripheral::new(pac.UART0, uart_pins, &mut pac.RESETS)
+		.enable(
+			CUSTOM_CONFIG,
+			clocks.peripheral_clock.freq(),
+		)
+		.unwrap();
 	loop {
-		delay.delay_ms(value);
-		led.set_high();
-		delay.delay_ms(value);
-		led.set_low();
+		uart.write_full_blocking(b"Hello World\n");
 	}
 }
