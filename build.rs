8d07aaaaf990c200e494ddabe0420b0361478252
ArchUsr64 <ArchUsr64>
2022-11-05 15:48:03 +0530

    Removed build script

--- build.rs
+++
@@ -1,7 +0,0 @@
-// use std::process::Command;
-//
-fn main() {
-	// println!("cargo:rerun-if-changed=target/thumbv6m-none-eabi/release/egboard");
-	// Command::new("elf2uf2-rs")
-    println!("cargo:rustc-link=-Tdefmt.x");
-}
