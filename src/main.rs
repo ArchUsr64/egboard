6b684ec30967461e1ed8674261743b4ee2bfd292
--global <--global>
2023-04-15 23:02:18 +0530

    implemented a basic firmware

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,14 @@ use defmt::*;
 use defmt_rtt as _;
 use rp_pico::{entry, hal};
 
+use embedded_hal::digital::v2::*;
+use embedded_hal::prelude::*;
+use fugit::ExtU32;
+use usb_device::class_prelude::*;
+use usb_device::prelude::*;
+use usbd_human_interface_device::page::Keyboard;
+use usbd_human_interface_device::prelude::*;
+
 #[entry]
 fn main() -> ! {
 	use cortex_m::delay::Delay;
@@ -29,8 +37,8 @@ fn main() -> ! {
 	)
 	.ok()
 	.unwrap();
+	let timer = hal::Timer::new(pac.TIMER, &mut pac.RESETS);
 	let mut delay = Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
-	delay.delay_ms(100);
 	let sio = hal::Sio::new(pac.SIO);
 	let pins = rp_pico::Pins::new(
 		pac.IO_BANK0,
@@ -38,6 +46,28 @@ fn main() -> ! {
 		sio.gpio_bank0,
 		&mut pac.RESETS,
 	);
+	delay.delay_ms(1);
+	info!("Pins initialised");
+	let usb_bus = UsbBusAllocator::new(hal::usb::UsbBus::new(
+		pac.USBCTRL_REGS,
+		pac.USBCTRL_DPRAM,
+		clocks.usb_clock,
+		true,
+		&mut pac.RESETS,
+	));
+
+	let mut keyboard = UsbHidClassBuilder::new()
+		.add_interface(
+			usbd_human_interface_device::device::keyboard::NKROBootKeyboardConfig::default(),
+		)
+		.build(&usb_bus);
+
+	//https://pid.codes
+	let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x1209, 0x0001))
+		.manufacturer("ArchUsr64")
+		.product("egboard")
+		.serial_number("1234")
+		.build();
 
 	let mut led = pins.led.into_push_pull_output();
 	led.set_high().unwrap();
@@ -52,6 +82,12 @@ fn main() -> ! {
 
 	let mut previous_state: u64;
 	let mut state = 0;
+	let mut input_count_down = timer.count_down();
+	input_count_down.start(10.millis());
+
+	let mut tick_count_down = timer.count_down();
+	tick_count_down.start(1.millis());
+
 	loop {
 		previous_state = state;
 		state = 0;
@@ -67,6 +103,93 @@ fn main() -> ! {
 		if state != previous_state {
 			println!("{:040b}", state);
 		}
-		delay.delay_ms(50);
+		//Poll the keys every 10ms
+		if input_count_down.wait().is_ok() {
+			let keys = get_keys(state);
+
+			match keyboard.interface().write_report(keys) {
+				Err(UsbHidError::WouldBlock) => {}
+				Err(UsbHidError::Duplicate) => {}
+				Ok(_) => {}
+				Err(e) => {
+					core::panic!("Failed to write keyboard report: {:?}", e)
+				}
+			};
+		}
+
+		//Tick once per ms
+		if tick_count_down.wait().is_ok() {
+			match keyboard.interface().tick() {
+				Err(UsbHidError::WouldBlock) => {}
+				Ok(_) => {}
+				Err(e) => {
+					core::panic!("Failed to process keyboard tick: {:?}", e)
+				}
+			};
+		}
+
+		if usb_dev.poll(&mut [&mut keyboard]) {
+			match keyboard.interface().read_report() {
+				Err(UsbError::WouldBlock) => {
+					//do nothing
+				}
+				Err(e) => {
+					core::panic!("Failed to read keyboard report: {:?}", e)
+				}
+				Ok(leds) => {
+					led.set_state(PinState::from(leds.num_lock)).ok();
+				}
+			}
+		}
 	}
+}
+
+fn get_keys(state: u64) -> [Keyboard; 32] {
+	let key_state_i = |index: u64| state & (1 << index) != 0;
+	let bind_key = |index, key_event| {
+		if key_state_i(index) {
+			key_event
+		} else {
+			Keyboard::NoEventIndicated
+		}
+	};
+	use Keyboard::*;
+	[
+		bind_key(0, P),
+		bind_key(1, O),
+		bind_key(2, I),
+		bind_key(3, U),
+		bind_key(4, Y),
+		bind_key(5, T),
+		bind_key(6, R),
+		bind_key(7, E),
+		bind_key(8, W),
+		bind_key(9, Q),
+		bind_key(10, Semicolon),
+		bind_key(11, L),
+		bind_key(12, K),
+		bind_key(13, J),
+		bind_key(14, H),
+		bind_key(15, G),
+		bind_key(16, F),
+		bind_key(17, D),
+		bind_key(18, S),
+		bind_key(19, A),
+		bind_key(20, ForwardSlash),
+		bind_key(21, Dot),
+		bind_key(22, Comma),
+		bind_key(23, M),
+		bind_key(24, N),
+		bind_key(25, B),
+		bind_key(26, V),
+		bind_key(27, C),
+		bind_key(28, X),
+		bind_key(29, Z),
+		bind_key(34, Space),
+		if key_state_i(33) && key_state_i(36) {
+			DeleteBackspace
+		} else {
+			NoEventIndicated
+		},
+	]
 }
